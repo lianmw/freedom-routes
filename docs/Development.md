2a26729605652082fb92da58d908f5204780a4f7
Saber <sabersalv@gmail.com>
2015-12-24 13:45:17 +0800

    use cmroute.dll method to import routes in Windows. fix #9

---
+++ docs/Development.md
@@ -0,0 +1,10 @@
+```
+$ go get -u
+$ go install
+$ freedom-routes linux
+```
+Debug: $ freedom-routes
+
+Development: use SOURCE/templates
+Production: use /usr/share/freedom-routes
+            windows: current directory
--- routes/templates/windows/routes-up.bat
+++ routes/templates/windows/routes-up.bat
@@ -1,16 +1,13 @@
 @echo off
 
-for /F "tokens=3" %%* in ('route print ^| findstr "\<0.0.0.0\>"') do set "gw=%%*"
-
-IF %gw%==%%* (
-  echo 閿欒: 鏈兘鎵惧埌缃戝叧
+net session >nul 2>&1
+if not %errorlevel% == 0 (
+  echo 请使用右键 "已管理员身份运行" 此脚本
   pause
-  exit
+  exit 1
 )
 
-ipconfig /flushdns
-
-@echo on
-
-{{range $i, $ip := .Ips}}route add {{$ip.Ip}} mask {{$ip.Mask}} %gw% metric 5
-{{end}}
+cd %~dp0
+echo 导入路由表...
+rundll32.exe cmroute.dll,SetRoutes /STATIC_FILE_NAME add.txt /DONT_REQUIRE_URL /IPHLPAPI_ACCESS_DENIED_OK
+pause
